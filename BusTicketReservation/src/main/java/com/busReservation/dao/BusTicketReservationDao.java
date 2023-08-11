577f1aa40f437f7efc94a7e26c9c6d12329434c6
ARAVINDHARAJ7 <101449402+ARAVINDHARAJ7@users.noreply.github.com>
2023-08-11 11:09:30 +0530

    Add files via upload

---
+++ BusTicketReservation/src/main/java/com/busReservation/dao/BusTicketReservationDao.java
@@ -0,0 +1,36 @@
+package com.busReservation.dao;
+
+import java.util.List;
+
+import com.busReservation.entity.*;
+
+public interface BusTicketReservationDao {
+	
+	public void addUser(User user);
+	
+	public List<Admin> getAllAdmins();
+	
+	public boolean isValidAdmin(String adminName,String password);
+	
+	public List<User> getAllUsers();
+	
+	public boolean isValidUser(String userName,String password);
+	
+	public void addBus(BusInfo busInfo);
+	
+	List<BusInfo> displayAllBuses();
+	
+	public void addBooking(BookingInfo bookingInfo);
+	
+	List<BookingInfo> getBookingById(int bookingId);
+	
+	public BusInfo getBusByNo(int busNo);
+	
+	public void updateBus(BusInfo busInfo);
+	
+	public void deleteBus(BusInfo busInfo);
+
+	
+		
+
+}
